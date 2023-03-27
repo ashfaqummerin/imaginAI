8005a3d843f7be95ab975f9bef650eea7e6f84a2
ashfaq_ummer <ashfaqummerin@gmail.com>
2023-03-27 15:08:20 +0530

    Cloudinary config

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -7,5 +7,10 @@ import Post from "../mongodb/models/post.js";
 dotenv.config();
 
 const router = express.Router();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export default router;
