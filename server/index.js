f8972d14958d0a4b35a131a08894e30562e4b862
ashfaq_ummer <ashfaqummerin@gmail.com>
2023-03-28 14:13:14 +0530

    Change in node modules

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import expres from "express";
+import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./mongodb/connect.js";
@@ -7,9 +7,9 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
-const app = expres();
+const app = express();
 app.use(cors());
-app.use(expres.json({ limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
